75071b082cafd717fac35d850957353f599eff59
Tom Donohue <monodot@gmail.com>
2023-03-29 20:42:40 +0100

    Add basic TF

---
+++ terraform-basic/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        grafana = {
+            source = "grafana/grafana"
+            version = "1.36.1"
+        }
+    }
+}
+
+provider "grafana" {
+    alias = "cloud"
+    cloud_api_key = "${var.grafana_cloud_api_key}"
+}
+
+resource "grafana_cloud_stack" "stack_1" {
+    provider = grafana.cloud
+    name = "Test Stack 1"
+    slug = "td77teststack1"
+}
+
+resource "grafana_cloud_stack" "stack_2" {
+    provider = grafana.cloud
+    name = "Test Stack 2"
+    slug = "td77teststack2"
+}
+
