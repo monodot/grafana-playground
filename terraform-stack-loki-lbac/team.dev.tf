14489a4d2ee07389c87442a6d2ecf7a67f70440f
Tom Donohue <monodot@gmail.com>
2024-02-19 20:47:14 +0000

    Colocate all the dev team stuff

--- terraform-stack-loki-lbac/team.dev.tf
+++ terraform-stack-loki-lbac/team.dev.tf
@@ -5,6 +5,35 @@ resource "grafana_team" "dev_team" {
   members  = []
 }
 
+resource "grafana_cloud_access_policy" "test" {
+  provider     = grafana.cloud
+  region       = "us"
+  name         = "lokilbacdemo-dev-only-policy"
+  display_name = "lokilbacdemo dev-only policy"
+
+  scopes = ["logs:read"]
+
+  realm {
+    type       = "org"
+    identifier = data.grafana_cloud_organization.current.id
+
+    label_policy {
+      selector = "{environment=\"development\"}"
+    }
+    label_policy {
+      selector = "{team=\"developers\"}"
+    }
+  }
+}
+
+resource "grafana_cloud_access_policy_token" "test" {
+  provider         = grafana.cloud
+  region           = "us"
+  access_policy_id = grafana_cloud_access_policy.test.policy_id
+  name             = "lokilbacdemo-dev-only-token"
+  display_name     = "lokilbacdemo dev-only Token"
+}
+
 resource "grafana_data_source" "loki_for_devs" {
   provider            = grafana.stack
   type                = "loki"
