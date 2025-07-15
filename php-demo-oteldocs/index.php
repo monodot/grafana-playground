c5e994848bec4692f741e7ae0de27416edf88e1c
Tom Donohue <monodot@gmail.com>
2025-07-15 15:21:50 +0100

    php shifting

---
+++ php-demo-oteldocs/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Monolog\Logger;
+use OpenTelemetry\API\Globals;
+use OpenTelemetry\Contrib\Logs\Monolog\Handler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LogLevel;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loggerProvider = Globals::loggerProvider();
+$handler = new Handler(
+    $loggerProvider,
+    LogLevel::INFO
+);
+$monolog = new Logger('otel-php-monolog', [$handler]);
+
+$app = AppFactory::create();
+
+$app->get('/rolldice', function (Request $request, Response $response) use ($monolog) {
+    $result = random_int(1,6);
+    $response->getBody()->write(strval($result));
+    $monolog->info('dice rolled', ['result' => $result]);
+    return $response;
+});
+
+$app->run();
