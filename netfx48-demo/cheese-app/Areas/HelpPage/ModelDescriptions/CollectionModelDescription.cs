487f2c231af2629f69f5544aca38f5716b2e6189
Tom Donohue <monodot@gmail.com@>
2025-11-14 10:12:12 +0000

    Add quickstart .NET Framework 4.8 source code

---
+++ netfx48-demo/cheese-app/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace cheese_app.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
