d7690630a72a61e4539c1817f5ca934ba985bf4d
Tom Donohue <monodot@gmail.com>
2025-11-28 10:22:51 +0000

    feat: add OpenTelemetry instrumentation with Grafana support
    - Add `Grafana.OpenTelemetry` and `OpenTelemetry.Exporter.Console` packages to the project.
    - Integrate OpenTelemetry tracing, metrics, and logging with Grafana and console exporters in `Program.cs`.

--- dotnet-kafka-otel/Program.cs
+++ dotnet-kafka-otel/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using Grafana.OpenTelemetry;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .UseGrafana()
+    .AddConsoleExporter()
+    .Build();
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .UseGrafana()
+    .AddConsoleExporter()
+    .Build();
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(logging =>
+    {
+        logging.UseGrafana()
+            .AddConsoleExporter();
+    });
+});
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
