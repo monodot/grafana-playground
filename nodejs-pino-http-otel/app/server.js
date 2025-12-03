553da2da0db8f3e545f99e49c52bf24d1046a67b
Tom Donohue <monodot@gmail.com>
2025-12-03 21:32:15 +0000

    feat: add simple pino-http request/response logging

--- nodejs-pino-http-otel/app/server.js
+++ nodejs-pino-http-otel/app/server.js
@@ -5,10 +5,19 @@ const port = 3001;
 
 const logger = pino();
 
+const logger2 = require('pino-http')();
+
 app.get('/', (req, res) => {
-    logger.info('Received / request');
+    logger.info('GET /');
+    logger2(req, res);
     res.send('Hello World!')
 });
+
+app.post('/', (req, res) => {
+    logger.info('POST /');
+    logger2(req, res);
+    res.send('Thanks for the input!');
+})
 
 app.listen(port, () => {
     logger.info(`Example app listening on port ${port}`)
